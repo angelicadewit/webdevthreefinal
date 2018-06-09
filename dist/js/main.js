08f3242751b432820441a07b576514b6771a8a56
Angelica de Wit <angelicadewit@Angelicas-MacBook-Pro.local>
2018-06-08 23:42:00 -0400

    changed directories and added the icons to the dropdown

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,12 +2,15 @@
 
 document.querySelector(".accordion").addEventListener("click", function () {
     var panel = document.querySelector(".panel");
+    var span = document.querySelector(".showMore");
     if (panel.classList.contains("close")) {
         panel.classList.add("open");
         panel.classList.remove("close");
+        span.innerHTML = "&#8211;";
     } else {
         panel.classList.add("close");
-        panel.classList.remove("open");
+        panel.classList.remove("add");
+        span.innerHTML = "&#43;";
     }
 });
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,14 @@
 
 document.querySelector(".accordion").addEventListener("click", function(){
     const panel = document.querySelector(".panel")
+    const span = document.querySelector(".showMore")
     if (panel.classList.contains("close")){
         panel.classList.add("open");
         panel.classList.remove("close");
+        span.innerHTML = "&#8211;"
     } else {
         panel.classList.add("close");
         panel.classList.remove("add");
+        span.innerHTML = "&#43;"
     }
 })
